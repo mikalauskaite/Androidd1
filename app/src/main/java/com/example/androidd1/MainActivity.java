0b27e7daac80c58297240bbb0bf53722dbed1ee8
Ema Mikalauskaitė <mikalauskaite.ema@gmail.com>
2021-12-14 22:47:49 +0200

    pridetas antras mygtukas

--- app/src/main/java/com/example/androidd1/MainActivity.java
+++ app/src/main/java/com/example/androidd1/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.androidd1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.tvMain = findViewById(R.id.tvMain);
+    }
+
+    public void onBtnClick(View view) {
+        tvMain.setText("Juk pažadėjau!");
     }
 }
